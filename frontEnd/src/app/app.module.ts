a49fda2d3fdf1ae3f0a7915e4f039d3d37cb1b64
pedroduarten9 <pedroduarteneto9@gmail.com>
2018-03-01 22:40:09 +0000

    pequena correçao

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AngularFireModule } from 'angularfire2';
 // for AngularFireAuth
