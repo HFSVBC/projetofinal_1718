68f3159ecb1502b03e43a2d192e0d5d8a8607824
pedroduarten9 <pedroduarteneto9@gmail.com>
2018-03-02 22:19:45 +0000

    [SIMDADOS] mini demo ate decisao final

---
+++ demoSim/createsim.py
@@ -0,0 +1,6 @@
+with open('create.sql', 'w') as infile:
+    infile.write("CREATE TABLE Aluno (ID int PRIMARY KEY, NumAluno int, PrimeiroNome varchar(255), UltimoNome varchar(255));\n")
+    infile.write("CREATE TABLE Sala (ID int PRIMARY KEY, Bloco int, Piso int, Numero int);\n")
+
+    for i in range(1,16):
+        infile.write("CREATE TABLE Semana" + str(i) + " (ID int PRIMARY KEY, IDSala int, IDAluno int, FOREIGN KEY (IDSala) REFERENCES Sala(ID),FOREIGN KEY (IDAluno) REFERENCES Aluno(ID));\n")
---
+++ demoSim/populatesim.py
@@ -0,0 +1,21 @@
+with open('populate.sql', 'w') as infile:
+
+    #Criar alunos
+    nomes = ["Jose", "Rita", "Ana", "Moniz", "Marta", "Tomas", "Andre", "Fernanda", "Alberto"]
+    ultimo_nome = ["Andrade", "Sousa", "Martins", "Balas", "Simoes", "Santos", "Rico", "Tina", "Ribeiro"]
+    num_aluno = [12345, 24735, 34698, 48583, 43278, 50124, 273853, 43829, 482917]
+
+    for i in range(1, len(nomes)+1):
+        infile.write("INSERT INTO Aluno VALUES(" + str(i) + "," + str(num_aluno[i-1]) + "," + nomes[i-1] + "," + ultimo_nome[i-1] + ");\n")
+
+    #Criar salas
+    bloco = [1,2,3,4,5]
+    piso = [1,2,3,1,1]
+    numero = [23,13,55,23,12]
+
+    for i in range(1, len(bloco)+1):
+        infile.write("INSERT INTO Sala(" + str(i) + "," + str(bloco[i-1]) + "," + str(piso[i-1]) + "," + str(numero[i-1]) + ");\n")
+
+    #Criar acessos
+    for i in range(1, 6):
+        infile.write("INSERT INTO Semana" + str(i) + "(" + str(i) + "," + str(i) + "," + str(i) + ");\n")
