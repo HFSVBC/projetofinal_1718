ef8a918319186c7b4587533a0f07bdf36ca0daac
Patrícia Alexandra Dias De Jesus <fc46593@alunos.fc.ul.pt>
2018-04-16 22:36:48 +0100

    [FRONTEND - admin]

--- frontEnd/src/app/admin/admin.component.ts
+++ frontEnd/src/app/admin/admin.component.ts
@@ -11,6 +11,8 @@ import { EmailValidator } from '@angular/forms';
 export class AdminComponent implements OnInit {
   email;
   user;
+  users;
+  u_number;
   user_name;
   user_email;
   user_uid;
@@ -36,22 +38,53 @@ export class AdminComponent implements OnInit {
       console.log('user', res);
       this.user = res['data']['user'];
       this._cookieService.put('token', res['data']['token']);
-      this.user_name = this.user['name'];
-      console.log('user', this.user);
-      this.user_email = this.user['email'];
-      this.user_uid = this.user['uid'];
-      this.user_type = this.user['user_type'];
-      this.user_avatar = this.user['avatar'];
-      console.log(res);
+      this.user_name = this.user.name;
+      this.user_email = this.user.email;
+      this.user_uid = this.user.uid;
+      this.user_type = this.user.user_type;
+      this.user_avatar = this.user.avatar;
     });
+
+    this.users = [{
+      "id": 0,
+      "type": "student"
+    }, {
+      "id": 1,
+      "type": "teacher"
+    },
+    {
+      "id": 2,
+      "type": "staff member"
+    },
+    {
+      "id": 3,
+      "type": "security"
+    }, {
+      "id": 10,
+      "type": "admin"
+    },
+    {
+      "id": 20,
+      "type": "all"
+    },
+    {
+      "id": 30,
+      "type": "mine"
+    }
+    ];
   }
 
-  changeType() {
-    const url = this.apiconnector.retriveprofilePOST;
-    const data = new FormData();
-    data.append('userEmail', this.email);
+  onChange(newValue) {
+    for (let user of this.users) {
+      if (user.type == newValue) {
+        const url = this.apiconnector.retriveprofilePOST;
+        const data = new FormData();
+        data.append('userTokenId', this._cookieService.get('token'));
+        data.append('userEmail', this.email);
+        data.append('user_type', this.user_type);
 
-    this.apiconnector.postData(url, data);
+        this.apiconnector.postData(url, data)
+      }
+    }
   }
-
 }
