7713b5e853c733e24140911d9863365f50d1b9d4
Hugo Curado <hfsvbc@hugocurado.info>
2018-05-04 20:42:20 +0100

    small change on allowed headers

--- backEnd/application/controllers/History.php
+++ backEnd/application/controllers/History.php
@@ -6,7 +6,7 @@ class History extends CI_Controller {
 	public function __construct($config = 'rest')
 	{
 		header('Access-Control-Allow-Origin: *');
-		header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+		header("Access-Control-Allow-Methods: GET, HEAD, OPTIONS, PUT, POST, PATCH, DELETE");
 		// header("Access-Control-Allow-Headers: Content-Type");
 		parent::__construct();
 
