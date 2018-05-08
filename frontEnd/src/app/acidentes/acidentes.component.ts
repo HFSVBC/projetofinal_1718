64782922facf92c13394c7bbd1734286f0a40125
Andre Nunes <andrenunes103@gmail.com>
2018-05-08 01:40:52 +0100

    FRONTEND - form validator

--- frontEnd/src/app/acidentes/acidentes.component.ts
+++ frontEnd/src/app/acidentes/acidentes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 class Acidente {
   tipo: string;
@@ -15,10 +16,24 @@ class Acidente {
 })
 export class AcidentesComponent implements OnInit {
   model = new Acidente();
+  tipos = ['Perda', 'Furto', 'Incêndio', 'Inundação', 'Outro'];
+  a = 'T';
+  acidenteForm;
 
   constructor() { }
 
   ngOnInit() {
+    this.acidenteForm = new FormGroup({
+      'tipo': new FormControl(this.model.tipo, Validators.required),
+      'espaco': new FormControl(this.model.espaco, Validators.pattern('[1-8].[1-5].[1-9]*')),
+      'data': new FormControl(this.model.data, Validators.required)
+    });
+
+  }
+
+  newModel() {
+    this.model = new Acidente();
+    console.log('Modelo', this.model);
   }
 
   onSubmit() {
