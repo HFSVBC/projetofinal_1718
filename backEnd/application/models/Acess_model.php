e5c29d7eb72dd0a1c92d93161e0d5614898c9565
AnaC-Sousa <ana.catarina.s@hotmail.com>
2018-05-09 19:33:09 +0100

    another fix

--- backEnd/application/models/Acess_model.php
+++ backEnd/application/models/Acess_model.php
@@ -8,7 +8,7 @@
 		//
 		public function getAcessByUser($uid)
 		{
-			$uid = $this->db->escape($u id);
+			$uid = $this->db->escape($uid);
 
 			$sql = "SELECT a.data_entrada, a.data_fim, e.bloco, e.piso, e.sala FROM acesso a,
 					 espaco e WHERE a.user=$uid AND a.espaco=e.id";
