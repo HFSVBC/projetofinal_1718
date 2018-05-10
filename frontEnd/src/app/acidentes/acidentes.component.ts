26fce512a3effe50de49ccd1484064eb82c986ba
Hugo Curado <hfsvbc@hugocurado.info>
2018-05-10 11:21:13 +0100

    some updates to frontend (still fixing some vulnerabilities) & datepicker working on 'acidentes'

--- frontEnd/src/app/acidentes/acidentes.component.ts
+++ frontEnd/src/app/acidentes/acidentes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import * as $ from 'jquery';
+import '@fengyuanchen/datepicker';
 
 class Acidente {
   tipo: string;
@@ -14,6 +16,7 @@ class Acidente {
   templateUrl: './acidentes.component.html',
   styleUrls: ['./acidentes.component.css']
 })
+
 export class AcidentesComponent implements OnInit {
   model = new Acidente();
   tipos = ['Perda', 'Furto', 'Incêndio', 'Inundação', 'Outro'];
@@ -28,9 +31,13 @@ export class AcidentesComponent implements OnInit {
       'espaco': new FormControl(this.model.espaco, Validators.pattern('[1-8].[1-5].[1-9]*')),
       'data': new FormControl(this.model.data, Validators.required)
     });
-
+    $(document).ready(function(){
+      (<any>$('[data-toggle="datepicker"]')).datepicker({
+        endDate: new Date(),
+      });
+    });
   }
-
+  
   newModel() {
     this.model = new Acidente();
     console.log('Modelo', this.model);
